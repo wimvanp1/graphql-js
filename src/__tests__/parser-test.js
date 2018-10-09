06a5abc1d69264ce10c682fe566852dbbca3e2ec
Wim Vanparijs <wim.vanparijs@vub.ac.be>
2018-10-09 09:09:49 +0200

    Initial test files for the interparameter constraint parser

---
+++ src/__tests__/parser-test.js
@@ -0,0 +1,38 @@
+/**
+ * @flow strict
+ */
+
+import { describe, it } from 'mocha';
+import { parse } from '../language/parser';
+
+describe('Interparameter Constraints Parser Tests', () => {
+  it('Correctly parses a schema without interparameter constraints', async () => {
+    const schema = `
+      type User{
+        id: String!,
+        name: String!
+      }
+    
+      type Query {
+        user(id: String, name: String): User
+      }
+    `;
+
+    parse(schema);
+  });
+
+  it('Correctly parses a query with an empty set of interparameter constraints', async () => {
+    const schema = `
+      type User{
+        id: String!,
+        name: String!
+      }
+    
+      type Query {
+        user(id: String, name: String){}: User
+      }
+    `;
+
+    parse(schema);
+  });
+});
