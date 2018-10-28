4ba9c08d21c19b257a6df66ae1ceef2a93a9efc2
Wim Vanparijs <wim.vanparijs@vub.ac.be>
2018-10-28 18:12:38 +0100

    Initial definition of two interparameter constraints (XOR and THEN)

---
+++ src/language/interparameterConstraint.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2018 Wim Vanparijs
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict
+ */
+
+import type { NameNode } from './ast';
+
+export const InterparameterConstraintOperator = Object.freeze({
+  XOR: 'XOR',
+  THEN: 'THEN',
+});
+
+export function isInterparameterConstraintOperator(name: NameNode) {
+  return InterparameterConstraintOperator.hasOwnProperty(name.value);
+}
