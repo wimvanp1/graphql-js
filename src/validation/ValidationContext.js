abc4a8781846bab1d8349f0f5977ae27d97c38b8
Wim Vanparijs <wim.vanparijs@vub.ac.be>
2018-11-06 22:05:15 +0100

    Added additional comments

--- src/validation/ValidationContext.js
+++ src/validation/ValidationContext.js
@@ -238,6 +238,7 @@ export class ValidationContext extends ASTValidationContext {
     return this._typeInfo.getParentInputType();
   }
 
+  // Returns the definition of the current field
   getFieldDef(): ?GraphQLField<*, *> {
     return this._typeInfo.getFieldDef();
   }
